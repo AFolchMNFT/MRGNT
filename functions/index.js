fd6dcd3b08c12f4b5f29320165525dc6035ddc2c
Alberto Folch <albertofolch@Albertos-MacBook-Air-2.local>
2026-08-04 11:05:06 -0600

    Add persistent dev environment: apiDev function, mrgnt-dev hosting target, updated deploy workflow

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,4 @@ const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } = require('./secrets');
 setGlobalOptions({ region: 'us-central1' });
 
 exports.api = onRequest({ invoker: 'public', secrets: [SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET] }, app);
+exports.apiDev = onRequest({ invoker: 'public', secrets: [SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET] }, app);
